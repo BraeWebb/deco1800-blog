b4bf9507f1addac53b6a4603b0b2ad8830c1b9ea
BraeWebb <email@braewebb.com>
2017-08-07 21:42:55 +1000

    Added RSS feed

---
+++ README.md
@@ -0,0 +1,4 @@
+Quick and Nasty Blog
+====================
+
+A quick and nastly blogging solution for the DECO1800 blog.
--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,31 @@ router.get('/tag/:tag', function(req, res, next){
     });
 });
 
+router.get('/edit/:post', function(req, res, next){
+    Post.findOne({"id": req.params.post}).exec(function(err, post){
+        if (err || !post){
+            return next();
+        }
+
+        res.render('editor', { title: 'Edit Blog Post', post: post, format: format});
+    })
+});
+
+router.get('/.rss', function(req, res, next){
+    Post.find({"author": "BRAE"}).sort({date: -1}).exec(function(err, posts) {
+        res.render('rss', {posts: posts});
+    });
+});
+
+router.get('/posts/:post', function(req, res, next){
+    Post.findOne({"id": req.params.post}).exec(function(err, post){
+        if (err || !post){
+            return next();
+        }
+
+        res.render('index', { title: post.title, posts: [post], format: format});
+    })
+});
+
+
 module.exports = router;
